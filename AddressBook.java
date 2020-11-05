c54a4d3f77d675599651481af99e208fb3549d44
codexakshay <codexakshay@gmail.com>
2020-11-05 15:57:10 +0530

    UC3_EditContact : User Can Edit Contact

--- AddressBook.java
+++ AddressBook.java
@@ -7,9 +7,11 @@ import java.util.Scanner;
 public class AddressBook {
 
 	public static List<Contact> ContactList = new ArrayList<Contact>();
-	Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
 
-	public void addContact() {
+	static int choice = 0;
+
+	private void addContact() {
 		System.out.println("Enter Contact Details");
 		System.out.println("Enter FirstName: ");
 		String firstName = sc.next();
@@ -26,13 +28,98 @@ public class AddressBook {
 		System.out.println("Enter zipCode: ");
 		String zipCode = sc.next();
 
-		Contact Contact1 = new Contact(firstName, lastName, address, city, state, mobileNum, zipCode);
-		ContactList.add(Contact1);
+		Contact Contact = new Contact(firstName, lastName, address, city, state, mobileNum, zipCode);
+		ContactList.add(Contact);
 		System.out.println("Contact details added successfully");
+	}
+
+	private void editContact() {
+		if (ContactList.isEmpty()) {
+			System.out.println("There are no contacts to print");
+		} else {
+			String address, city, state, mobileNum, zipCode;
+			int id;
+			for (Contact contact : ContactList) {
+				System.out.println("ID " + ContactList.indexOf(contact) + ":\n" + contact);
+			}
+			System.out.println("Enter ID of contact to edit: ");
+			id = sc.nextInt();
+			System.out.println(ContactList.get(id));
+			System.out.println(
+					"Please select the option to edit\n1.Address\n2.City\n3.State\n4.zipCode\n5.Mobile Number");
+			choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				System.out.println("Enter Address: ");
+				address = sc.nextLine();
+				ContactList.get(id).setAddress(address);
+				break;
+			case 2:
+				System.out.println("Enter City: ");
+				city = sc.nextLine();
+				ContactList.get(id).setCity(city);
+				break;
+			case 3:
+				System.out.println("Enter State: ");
+				state = sc.nextLine();
+				ContactList.get(id).setState(state);
+				break;
+			case 4:
+				System.out.println("Enter Zip Code: ");
+				zipCode = sc.nextLine();
+				ContactList.get(id).setZipCode(zipCode);
+				break;
+			case 5:
+				System.out.println("Enter Mobile Number: ");
+				mobileNum = sc.nextLine();
+				ContactList.get(id).setMobileNum(mobileNum);
+				break;
+			default:
+				System.out.println("Error!!");
+				editContact();
+			}
+		}
+	}
+
+	private void printContact() {
+		if (ContactList.isEmpty()) {
+			System.out.println("There are no contacts to print");
+		} else {
+			for (Contact contact : ContactList) {
+				System.out.println(contact);
+			}
+		}
+	}
+
+	private void deleteContact() {
+
 	}
 
 	public static void main(String args[]) {
 		AddressBook book = new AddressBook();
-		book.addContact();
+
+		while (choice <= 5) {
+			System.out.println(
+					"1.Add Contact\n2.Print contact details\n3.Edit contact details\n4.Delete contact details\n5.Exit ");
+			choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				book.addContact();
+				break;
+			case 2:
+				book.printContact();
+				break;
+			case 3:
+				book.editContact();
+				break;
+			case 4:
+				book.deleteContact();
+			case 5:
+				System.exit(0);
+			default:
+				System.out.println("Error!");
+			}
+		}
 	}
+
 }
--- Contact.java
+++ Contact.java
@@ -5,12 +5,12 @@ public class Contact {
 	public String lastName;
 	public String address;
 	public String city;
-	public  String state;
+	public String state;
 	public String mobileNum;
 	public String zipCode;
-	
-	public Contact(String firstName, String lastName,String address,String city,String state,String mobileNum,String zipCode)
-	{
+
+	public Contact(String firstName, String lastName, String address, String city, String state, String mobileNum,
+			String zipCode) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
