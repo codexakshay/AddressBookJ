a3a9f059d6bd3f334f16acbeabc61118e03aa8c7
codexakshay <codexakshay@gmail.com>
2020-11-05 15:46:38 +0530

    UC2_AddContact : User can Add Contact

--- Contact.java
+++ Contact.java
@@ -5,18 +5,74 @@ public class Contact {
 	public String lastName;
 	public String address;
 	public String city;
-	public String state;
+	public  String state;
 	public String mobileNum;
 	public String zipCode;
-
-	public Contact(String firstName, String lastName, String address, String city, String state, String mobileNum,
-			String zipCode) {
+	
+	public Contact(String firstName, String lastName,String address,String city,String state,String mobileNum,String zipCode)
+	{
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
 		this.city = city;
 		this.state = state;
 		this.mobileNum = mobileNum;
+		this.zipCode = zipCode;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getMobileNum() {
+		return mobileNum;
+	}
+
+	public void setMobileNum(String mobileNum) {
+		this.mobileNum = mobileNum;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
 		this.zipCode = zipCode;
 	}
 }
