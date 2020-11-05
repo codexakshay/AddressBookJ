8a8e9ca6b01c74cac6ed1bb2051cd49bce8907a8
codexakshay <codexakshay@gmail.com>
2020-11-05 15:30:24 +0530

    UC1_CreateContact : Created Contact

---
+++ AddressBook.java
@@ -0,0 +1,7 @@
+package com.blz.code;
+
+public class AddressBook {
+	public static void main(String args[]) {
+		System.out.println("WELCOME TO ADDRESS BOOK");
+	}
+}
---
+++ Contact.java
@@ -0,0 +1,22 @@
+package com.blz.code;
+
+public class Contact {
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String city;
+	public String state;
+	public String mobileNum;
+	public String zipCode;
+
+	public Contact(String firstName, String lastName, String address, String city, String state, String mobileNum,
+			String zipCode) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.mobileNum = mobileNum;
+		this.zipCode = zipCode;
+	}
+}
